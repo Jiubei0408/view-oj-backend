c16ad8a9a0060a29f5f50803a3415e14bf4cbee6
winebottle <757795210@qq.com>
2019-12-06 14:13:27 +0800

    优化url，添加牛客的用户名转uid
    把爬取的url中不必要的参数去除。
    添加了用户名转uid的爬虫

--- app/spiders/nowcoder_spider.py
+++ app/spiders/nowcoder_spider.py
@@ -7,11 +7,14 @@ from app.spiders.spider_http import SpiderHttp
 class NowcoderSpider(BaseSpider):
     def get_user_info(self, oj_username):
         username = oj_username.oj_username
+        uid = NowcoderSpider.get_id_by_username(username)
+        if uid:
+            username = uid
         index = 1
         data = []
         pre = []
         while 1:
-            url = 'https://ac.nowcoder.com/acm/contest/profile/{}/practice-coding?&pageSize=200&search=&statusTypeFilter=5&languageCategoryFilter=-1&orderType=DESC&page={}'.format(
+            url = 'https://ac.nowcoder.com/acm/contest/profile/{}/practice-coding?pageSize=200&statusTypeFilter=5&page={}'.format(
                 username, index)
             res = SpiderHttp().get(url=url)
             r = re.findall(r'/acm/problem/(\d+)', res.text)
@@ -35,6 +38,19 @@ class NowcoderSpider(BaseSpider):
             rating = DEFAULT_PROBLEM_RATING
         return {'rating': rating}
 
+    @staticmethod
+    def get_id_by_username(username):
+        url = 'https://www.nowcoder.com/search?type=all&query={}'.format(username)
+        res = SpiderHttp().get(url=url)
+        test = re.findall(r'/profile/(\d+)', res.text)
+        if not test:
+            return None
+        return test[0]
+
 
 if __name__ == '__main__':
-    print(NowcoderSpider().get_problem_info('20123'))
+    from app.models.oj_username import OJUsername
+
+    oj_username = OJUsername()
+    oj_username.oj_username = 'Kwords'
+    print(NowcoderSpider().get_user_info(oj_username))
